d9692d4fb25737c1275b749cde2cff1dfcc87491
Guinn <eguinjonathan08@gmail.com>
2025-06-03 04:41:17 +0700

    feat(main): reset project and init nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white text-teal-300">
+      <Text className="text-blue-300 text-6xl font-bold">Welcome</Text>
+    </View>
+  );
+}
