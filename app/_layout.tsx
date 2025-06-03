4324267f6fd6436491f1f779f264f4c6c481c5df
Guinn <eguinjonathan08@gmail.com>
2025-06-03 20:31:58 +0700

    feat(layout): create header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,30 @@
 import { Stack } from "expo-router";
 import "@/app/globals.css";
+import { Image, View, Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+        headerTitle: () => (
+          <View className="bg-secondary p-2 rounded-lg flex">
+            <Text className="text-xl font-bold text-primary">
+              VALIDASI BLOK
+            </Text>
+          </View>
+        ),
+        headerLeft: () => (
+          <Image
+            source={require("@/assets/images/company-logo.png")}
+            className="w-12 h-12 ml-3"
+          />
+        ),
+        headerStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    />
+  );
 }
