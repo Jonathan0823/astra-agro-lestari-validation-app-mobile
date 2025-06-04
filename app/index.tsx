b4605d0076a89bf5a99fc70ca1c05ca4fc1960f5
Guinn <eguinjonathan08@gmail.com>
2025-06-04 19:08:31 +0700

    feat(home): update styling

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,12 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-white p-4">
-      <View className="flex-1 rounded-xl overflow-hidden space-y-3">
+    <View className="flex-1 bg-white p-4 pb-10">
+      <View className="flex flex-col h-full gap-6">
         {/* Kotak 1 */}
         <View className="flex-1">
           <Link href="/validate" asChild>
-            <TouchableOpacity className="flex-1 w-full items-center justify-center bg-slate-200 border-2 border-slate-500">
+            <TouchableOpacity className="flex-1 w-full items-center justify-center bg-slate-200 border-2 border-slate-500 rounded-xl max-w-[550px]">
               <Text className="text-2xl font-bold text-slate-600">
                 Mulai Validasi
               </Text>
@@ -19,7 +19,7 @@ export default function Index() {
         {/* Kotak 2 */}
         <View className="flex-1">
           <Link href="/history" asChild>
-            <TouchableOpacity className="flex-1 w-full items-center justify-center bg-slate-200 border-2 border-slate-500">
+            <TouchableOpacity className="flex-1 w-full items-center justify-center bg-slate-200 border-2 border-slate-500 rounded-xl max-w-[550px]">
               <Text className="text-2xl font-bold text-slate-600">
                 Riwayat Validasi
               </Text>
@@ -30,7 +30,7 @@ export default function Index() {
         {/* Kotak 3 */}
         <View className="flex-1">
           <Link href="/sample" asChild>
-            <TouchableOpacity className="flex-1 w-full items-center justify-center bg-slate-200 border-2 border-slate-500">
+            <TouchableOpacity className="flex-1 w-full items-center justify-center bg-slate-200 border-2 border-slate-500 rounded-xl max-w-[550px]">
               <Text className="text-2xl font-bold text-slate-600">
                 Data Sample
               </Text>
