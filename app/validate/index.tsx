cfb29edd6d3a4d161d28a6bd94c79bd8a63bfd75
Guinn <eguinjonathan08@gmail.com>
2025-06-04 14:27:14 +0700

    feat(validate): init validate page

--- app/validate/index.tsx
+++ app/validate/index.tsx
@@ -1,9 +1,32 @@
+import db from "@/firebaseConfig";
+import { User } from "@/types/User";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
 const Index = () => {
+  const [message, setMessage] = useState<User[]>([]);
+  useEffect(() => {
+    const testfireStorage = async () => {
+      try {
+        const query = await getDocs(collection(db, "users"));
+        const result = query.docs.map((doc) => ({
+          id: doc.id,
+          username: doc.data().username,
+        }));
+        console.log("Data from Firestore:", result);
+        setMessage(result);
+      } catch (err) {
+        console.error("Error in testfireStorage:", err);
+      }
+    };
+
+    testfireStorage();
+  }, []);
   return (
-    <View>
-      <Text></Text>
+    <View className="flex-1 p-2">
+      <Text>{message[0]?.id}</Text>
+      <Text>{message[0]?.username}</Text>
     </View>
   );
 };
