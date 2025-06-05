791d9e31e4977c302c93a1af0222fb903b840e4d
Guinn <eguinjonathan08@gmail.com>
2025-06-05 23:57:28 +0700

    feat(history): implement preview by using validationpreview comp

--- app/history/[id]/index.tsx
+++ app/history/[id]/index.tsx
@@ -1,9 +1,15 @@
+import ValidationPreview from "@/components/ValidationPreview";
+import { useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
 
 const Index = () => {
+  const { id } = useLocalSearchParams();
   return (
-    <View>
-      <Text></Text>
+    <View className="flex-1 bg-white p-2">
+      <Text className="font-bold text-primary text-2xl my-2 mb-4">
+        Blok {id}
+      </Text>
+      <ValidationPreview id={String(id)} />
     </View>
   );
 };
