f98db0ecd69bd3f71f40622df049135af94ce095
Guinn <eguinjonathan08@gmail.com>
2025-06-05 22:52:12 +0700

    feat(type): add sampledata type

---
+++ types/SampleData.ts
@@ -0,0 +1,11 @@
+export type SampleData = {
+  id?: number;
+  blok: number;
+  nomorBaris: string;
+  imageUri: string;
+  circle: boolean;
+  gawangan: boolean;
+  pruning: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
