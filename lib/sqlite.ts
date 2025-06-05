ebadab6557131bc03f26de65702f5939323fd1b3
Guinn <eguinjonathan08@gmail.com>
2025-06-05 22:51:59 +0700

    feat(sqlite): add some function related to sqlite

---
+++ lib/sqlite.ts
@@ -0,0 +1,45 @@
+import { SampleData } from "@/types/SampleData";
+import { SQLiteDatabase } from "expo-sqlite";
+
+export const getDataByBlok = async (
+  blok: number,
+  db: SQLiteDatabase,
+): Promise<any[]> => {
+  try {
+    const rows = await db.getAllAsync(
+      `SELECT * FROM sample_data WHERE blok = ? ORDER BY created_at DESC`,
+      [blok],
+    );
+    return rows;
+  } catch (error) {
+    console.error("Error fetching data by blok:", error);
+    throw error;
+  }
+};
+
+export const insertData = async (db: SQLiteDatabase, data: SampleData) => {
+  const circleInt = data.circle ? 1 : 0;
+  const gawanganInt = data.gawangan ? 1 : 0;
+  const pruningInt = data.pruning ? 1 : 0;
+
+  try {
+    await db.runAsync(
+      `INSERT INTO sample_data 
+      (blok, nomor_baris, image_uri, circle, gawangan, pruning, created_at, updated_at) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        data.blok,
+        data.nomorBaris,
+        data.imageUri,
+        circleInt,
+        gawanganInt,
+        pruningInt,
+        data.createdAt,
+        data.updatedAt,
+      ],
+    );
+  } catch (error) {
+    console.error("Error inserting data:", error);
+    throw error;
+  }
+};
