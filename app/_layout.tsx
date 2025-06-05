371f38472a642f53914357c12f815068b594e737
Guinn <eguinjonathan08@gmail.com>
2025-06-05 10:25:34 +0700

    feat(layout): add auth check and push to login if user is not logged in

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,32 @@
-import { Stack, useSegments } from "expo-router";
+import { router, Stack, useSegments } from "expo-router";
 import "@/app/globals.css";
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, TouchableOpacity, Alert } from "react-native";
 import { useEffect, useState } from "react";
+import * as SecureStore from "expo-secure-store";
+import { User } from "@/types/User";
 
 export default function RootLayout() {
   const [headerName, setHeaderName] = useState("VALIDASI BLOK");
+  const [user, setUser] = useState<User>();
   const segments = useSegments() as string[];
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const user = await SecureStore.getItemAsync("user"); // misalnya simpan token atau ID
+      if (user) {
+        console.log("User is logged in, setting user state");
+        setUser(JSON.parse(user)); // parse user data
+      } else {
+        console.log("No user found in SecureStore");
+      }
+      if (!user) {
+        console.log("User not logged in, redirecting to login page");
+        router.replace("/login"); // ganti halaman langsung ke login
+      }
+    };
+
+    checkLogin();
+  }, [segments]);
 
   useEffect(() => {
     if (segments.includes("validate")) {
@@ -18,6 +39,31 @@ export default function RootLayout() {
       setHeaderName("VALIDASI BLOK");
     }
   }, [segments]);
+
+  const handleClickUser = () => {
+    if (!user) {
+      router.push("/login");
+      return;
+    }
+
+    Alert.alert(
+      "Logout",
+      "Apakah kamu yakin ingin logout?",
+      [
+        { text: "Batal", style: "cancel" },
+        {
+          text: "Logout",
+          style: "destructive",
+          onPress: async () => {
+            await SecureStore.deleteItemAsync("user");
+            setUser(undefined);
+            router.replace("/login");
+          },
+        },
+      ],
+      { cancelable: true },
+    );
+  };
 
   return (
     <Stack
@@ -34,6 +80,17 @@ export default function RootLayout() {
             source={require("@/assets/images/company-logo.png")}
             className="w-12 h-12 ml-3"
           />
+        ),
+        headerRight: () => (
+          <TouchableOpacity onPress={handleClickUser}>
+            <View
+              className={`flex-row items-center mr-3 ${segments.length === 0 ? "w-20" : ""} `}
+            >
+              <Text className="text-md font-semibold text-gray-600">
+                {user?.username || "SIGN IN"}
+              </Text>
+            </View>
+          </TouchableOpacity>
         ),
         headerStyle: {
           backgroundColor: "white",
